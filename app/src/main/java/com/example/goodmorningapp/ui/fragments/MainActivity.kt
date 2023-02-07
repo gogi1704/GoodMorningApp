f351652218c3bed7378991a3876efa2755b30fde
George <96864675+gogi1704@users.noreply.github.com>
2023-02-07 18:39:06 +0300

    start create navigation

--- app/src/main/java/com/example/goodmorningapp/ui/fragments/MainActivity.kt
+++ app/src/main/java/com/example/goodmorningapp/ui/fragments/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.goodmorningapp
+package com.example.goodmorningapp.ui.fragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.goodmorningapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
